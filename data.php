ff47a535f13c6cb367f53d54785a4b55995e24e5
githubNUL <641570479@qq.com>
2018-06-20 23:20:12 +0800

    init ..

---
+++ data.php
@@ -0,0 +1,8 @@
+<?php   
+
+$results[0]=array("id"=>"1","full_name"=>"张三","money"=>"100");  
+$results[1]=array("id"=>"2","full_name"=>"李四","money"=>"200");  
+$results[2]=array("id"=>"3","full_name"=>"王五","money"=>"300");  
+echo json_encode($results);  
+
+?>
---
+++ welcome.php
@@ -0,0 +1,4 @@
+Hi <?php echo $_POST['userName']; ?>
+<br>
+	your passwd is <?php echo $_POST['inputPassword']; ?>
+<br>
